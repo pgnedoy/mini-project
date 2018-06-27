0deb7437c759e0073823f0ba3946578ad2a72bf8
Device UPTech <deviceuptech@Devices-MacBook-Air.local>
2018-06-27 13:26:16 +0300

    create marcup

---
+++ markup/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Marcup from './components/Marcup'
+
+class App extends Component {
+  render() {
+    return (
+      <Marcup />
+    );
+  }
+}
+
+export default App;
---
+++ markup/src/components/Marcup.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+import './marcup-style.css'
+
+class Marcup extends Component {
+  
+  render() {
+      return (
+    
+        <div class="grid-container">
+            <div class="item1">Stiky  Header</div>
+            <div class="item2">Block A</div>
+            <div class="item3">Block C</div>  
+            <div class="item4">Block B</div>
+            <div class="item5">Block D</div>
+            <div class="item6">Block E</div>
+            
+            <div class="item7">Footer</div>
+        </div> 
+      )
+    
+  }
+}
+
+
+
+export default Marcup;
