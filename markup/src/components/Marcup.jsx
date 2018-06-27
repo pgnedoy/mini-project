9951de03bd9a7b9fb244804e884597c8e92b00cb
Device UPTech <deviceuptech@Devices-MacBook-Air.local>
2018-06-27 15:54:14 +0300

    ready marcup

--- markup/src/components/Marcup.jsx
+++ markup/src/components/Marcup.jsx
@@ -6,17 +6,18 @@ class Marcup extends Component {
   
   render() {
       return (
-    
-        <div class="grid-container">
-            <div class="item1">Stiky  Header</div>
-            <div class="item2">Block A</div>
-            <div class="item3">Block C</div>  
-            <div class="item4">Block B</div>
-            <div class="item5">Block D</div>
-            {/* <div class="emptyItem"></div> */}
-            <div class="item6">Block E</div>
-            <div class="item7">Footer</div>
-        </div> 
+        
+            <div class="grid-container">
+                <div class="item1">Stiky  Header</div>
+                <div class="item2">Block A</div>
+                <div class="item3">Block C</div>  
+                <div class="item4">Block B</div>
+                <div class="item5">Block D</div>
+                {/* <div class="emptyItem"></div> */}
+                <div class="item6">Block E</div>
+                <div class="item7">Footer</div>
+            </div> 
+
       )
     
   }
